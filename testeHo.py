f715b9bc258d7096ea16b8a3238243e4a785063c
canavesix <122987490+canavesix@users.noreply.github.com>
2023-10-03 09:36:51 -0300

    Add files via upload

---
+++ testeHo.py
@@ -0,0 +1,15 @@
+andar = 1
+while andar <= 20:
+    if andar != 13:
+        print(andar)
+    andar += 1
+
+
+for andar in range(1, 21):
+    if andar != 13:
+        print(andar)
+
+
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)        
